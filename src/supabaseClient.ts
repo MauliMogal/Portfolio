9bac8d6c5f745a71007f162e5c6ee737b1af1894
Mauli Mogal <maulimogal@gmail.com>
2025-04-23 00:36:17 +0530

    supabase bug fix

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,11 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
+
+console.log('Supabase URL:', supabaseUrl);
+console.log('Supabase Key:', supabaseAnonKey);
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
-
-console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('Supabase Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-
-
